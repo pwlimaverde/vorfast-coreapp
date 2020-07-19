5e2c8da07c0fe120743487c017e8b59168636cf7
PWLIMAVERDE <vorfast@gmail.com>
2020-07-19 14:10:14 -0300

    ajustes-na-edicao-das-cores-do-header

---
+++ test/app/modules/home/componentes/container_edit_cor/container_edit_cor_widget_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_modular/flutter_modular_test.dart';
+
+import 'package:coreapp/app/modules/home/componentes/container_edit_cor/container_edit_cor_widget.dart';
+
+void main() {
+//  testWidgets('ContainerEditCorWidget has message', (tester) async {
+//    await tester.pumpWidget(buildTestableWidget(ContainerEditCorWidget()));
+//    final textFinder = find.text('ContainerEditCor');
+//    expect(textFinder, findsOneWidget);
+//  });
+}
